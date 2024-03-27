bc83a56c0331d7651030540e43fcae61664ede10
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2024-03-27 20:14:28 +0100

    zad10.4 !updated package auto-slider! (home)

--- html/js/auto-slider.js
+++ html/js/auto-slider.js
@@ -1,36 +1,39 @@
 const activeItemClass = 'slider-img-active';
-const items = document.getElementsByClassName('slider-img');
 const nextItem = document.getElementById('slider-next');
 const previousItem = document.getElementById('slider-prev');
+const slides = document.getElementsByClassName('slider-img');
+
 var index = 0;
 var intervalId = -1;
 var switchDelay = 5000;
 
+//document.addEventListener('keydown', keyDown);
+
 
 function nextSlide() {
 	let started = stopPresentation();
-	items[index].classList.remove(activeItemClass);
+	slides[index].classList.remove(activeItemClass);
 
-	if(index < items.length - 1){
+	if(index < slides.length - 1){
 		index++;
 	} else{
 		index = 0;
 	}
 
-	items[index].classList.add(activeItemClass);
+	slides[index].classList.add(activeItemClass);
 	if(started) startPresentation(switchDelay);
 }
 function prevSlide() {
 	let started = stopPresentation();
-	items[index].classList.remove(activeItemClass);
+	slides[index].classList.remove(activeItemClass);
 
 	if(index > 0){
 		index--;
 	} else{
-		index = items.length - 1;
+		index = slides.length - 1;
 	}
 
-	items[index].classList.add(activeItemClass);
+	slides[index].classList.add(activeItemClass);
 	console.log(index);
 	if(started) startPresentation(switchDelay);
 }
@@ -47,7 +50,7 @@ function keyDown(e) {
 
 function startPresentation(delay) {
 	if(intervalId == -1){
-		intervalId = setInterval("nextSlide()", delay);
+		intervalId = setInterval('nextSlide()', delay);
 		return intervalId;
 	}
 	else console.log('Presentation has been already started');
@@ -62,6 +65,3 @@ function stopPresentation() {
 	}
 	return false;
 }
-
-
-//document.addEventListener('keydown', keyDown);
