ab2f89f55a8b847555c1e1bfc193d2320b33c49a
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2024-05-21 16:26:39 +0200

    fixed user.html, testajax/index.html ; finish lab12

--- html/testajax/js/skrypty.js
+++ html/testajax/js/skrypty.js
@@ -4,7 +4,7 @@ const xhr = new XMLHttpRequest();
 function pobierzDane(nazwaPliku)
 {
 	if (xhr) {
-		var url = "http://localhost/testajax/dane/" + nazwaPliku + ".txt";
+		var url = "dane/" + nazwaPliku + ".txt";
 		xhr.open("GET", url);
 		xhr.addEventListener("readystatechange", function () {
 			if (xhr.readyState === 4) {
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	//obsługa zdarzenia kliknięcia na b1:
 	var but1 = document.getElementById("b1");
 	but1.addEventListener('click', function(){
-		fetch("http://localhost/testajax/dane/info.txt")
+		fetch("dane/info.txt")
 		.then( response => {return response.text();} )
 		.then( dane => { document.getElementById("s1").innerHTML = dane; })
 	}, false);
@@ -29,21 +29,21 @@ document.addEventListener("DOMContentLoaded", function() {
 	//obsługa zdarzenia kliknięcia na b2:
 	var but2 = document.getElementById("b2");
 	but2.addEventListener('click', function(){
-		fetch("http://localhost/testajax/dane/act.txt")
+		fetch("dane/act.txt")
 		.then( response => {return response.text();} )
 		.then( dane => { document.getElementById("s1").innerHTML = dane; })
 	}, false);
 
 	var but3 = document.getElementById("b3");
 	but3.addEventListener('click', function(){
-		fetch("http://localhost/testajax/dane/gal.txt")
+		fetch("dane/gal.txt")
 		.then( response => {return response.text();} )
 		.then( dane => { document.getElementById("s1").innerHTML = dane; })
 	}, false);
 	
 	var but4 = document.getElementById("b4");
 	but4.addEventListener('click', function(){
-		fetch("http://localhost/testajax/dane/form.txt")
+		fetch("dane/form.txt")
 		.then( response => {return response.text();} )
 		.then( dane => { document.getElementById("s1").innerHTML = dane; })
 	}, false);
